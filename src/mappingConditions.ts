7ca986ebab23b2bb0fbd0044b95e6224364347a8
Santiago Gonzalez Toral <hernangt12re3@gmail.com>
2020-01-05 23:24:32 -0500

    :bug: Fix handling missing SEAs

--- src/mappingConditions.ts
+++ src/mappingConditions.ts
@@ -8,37 +8,43 @@ export function handleFulfilledAccessSecretStoreCondition(event: AccessSecretSto
 	log.info("==== **** EVENT handleFulfilledAccessSecretStoreCondition ==== ****", [])
 	let agreementId = event.params._agreementId
 	let sea = ServiceAgreement.load(agreementId.toHex())
-	let condition = new AccessSecretStoreCondition(event.params._conditionId.toHex())
-	condition.agreement = sea.id
-	condition.documentId = event.params._documentId
-	condition.grantee = event.params._grantee
-	condition.conditionId = event.params._conditionId.toHex()
-	condition.lastBlockUpdated = event.block.number
-	condition.save()
+	if (sea != null) {
+		let condition = new AccessSecretStoreCondition(event.params._conditionId.toHex())
+		condition.agreement = sea.id
+		condition.documentId = event.params._documentId
+		condition.grantee = event.params._grantee
+		condition.conditionId = event.params._conditionId.toHex()
+		condition.lastBlockUpdated = event.block.number
+		condition.save()
+	}
 }
 
 export function handleFulfilledLockRewardCondition(event: LockRewardConditionFulFilled): void {
 	log.info("==== **** EVENT handleFulfilledLockRewardCondition ==== ****", [])
 	let agreementId = event.params._agreementId
 	let sea = ServiceAgreement.load(agreementId.toHex())
-	let condition = new LockRewardCondition(event.params._conditionId.toHex())
-	condition.agreement = sea.id
-	condition.rewardAddress = event.params._rewardAddress
-	condition.amount = event.params._amount
-	condition.conditionId = event.params._conditionId.toHex()
-	condition.lastBlockUpdated = event.block.number
-	condition.save()
+	if (sea != null) {
+		let condition = new LockRewardCondition(event.params._conditionId.toHex())
+		condition.agreement = sea.id
+		condition.rewardAddress = event.params._rewardAddress
+		condition.amount = event.params._amount
+		condition.conditionId = event.params._conditionId.toHex()
+		condition.lastBlockUpdated = event.block.number
+		condition.save()
+	}
 }
 
 export function handleFulfilledEscrowRewardCondition(event: EscrowRewardConditionFulfilled): void {
 	log.info("==== **** EVENT handleFulfilledEscrowRewardCondition ==== ****", [])
 	let agreementId = event.params._agreementId
 	let sea = ServiceAgreement.load(agreementId.toHex())
-	let condition = new EscrowRewardCondition(event.params._conditionId.toHex())
-	condition.agreement = sea.id
-	condition.receiver = event.params._receiver
-	condition.amount = event.params._amount
-	condition.conditionId = event.params._conditionId.toHex()
-	condition.lastBlockUpdated = event.block.number
-	condition.save()
+	if (sea != null) {
+		let condition = new EscrowRewardCondition(event.params._conditionId.toHex())
+		condition.agreement = sea.id
+		condition.receiver = event.params._receiver
+		condition.amount = event.params._amount
+		condition.conditionId = event.params._conditionId.toHex()
+		condition.lastBlockUpdated = event.block.number
+		condition.save()
+	}
 }
